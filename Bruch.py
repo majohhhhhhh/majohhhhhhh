2eac8acc61a6d43ef9b817031c8581591618a35a
mano <manolis.john@freenet.de>
2022-06-03 17:33:54 +0200

    Multiplikation

--- Bruch.py
+++ Bruch.py
@@ -13,9 +13,16 @@ class bruch():
         if a==b:
             self.z=self.z//a
             self.n=self.n//a
+            
+    def __mul__(self,other):
+        return bruch(self.z*other.z,self.n*other.n)
     
     def __str__(self) :
         return str(self.z)+'/'+str(self.n)
     
 x=bruch(21,15)
+y=bruch(2,7)
 print(x)
+print(y)
+print(x*y)
+
