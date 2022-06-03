00a88ca155362c0ff6210b66c4f5fe6ccfdeee3c
mano <manolis.john@freenet.de>
2022-06-03 17:30:43 +0200

    Addition

--- Bruch.py
+++ Bruch.py
@@ -13,9 +13,16 @@ class bruch():
         if a==b:
             self.z=self.z//a
             self.n=self.n//a
+            
+    def __add__(self,other):
+        return bruch(self.z*other.n+self.n*other.z,self.n*other.n)
+    
     
     def __str__(self) :
         return str(self.z)+'/'+str(self.n)
     
-x=bruch(21,15)
+x=bruch(15,21)
+y=bruch(12,16)
 print(x)
+print(y)
+print(x+y)
